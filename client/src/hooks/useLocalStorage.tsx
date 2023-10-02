5476ac60ab17d0d552ca8bc3c6c658628a5a3fb1
illiCristian <svrk73@gmail.com>
2023-10-02 14:41:42 -0300

    fixed localstorage

--- client/src/hooks/useLocalStorage.tsx
+++ client/src/hooks/useLocalStorage.tsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export function useLocalStorage<T>(item: string) {
-  const storedValue = localStorage.getItem(item);
-  const initialValue = storedValue ? JSON.parse(storedValue) : null;
+export function useLocalStorage<T>(item: string, initialValue: T) {
+  const [value, setValue] = useState<T>(initialValue);
 
-  const [value, setValue] = useState(initialValue);
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    let value = localStorage.getItem(item);
+    if (value) setValue(JSON.parse(value));
+  }, []);
 
   const updateLocalStorage = (newValue: T) => {
     setValue(newValue);
     localStorage.setItem(item, JSON.stringify(newValue));
   };
+
   return {
     value,
     updateLocalStorage,
